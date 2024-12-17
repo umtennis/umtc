e9e9827df8bb98dd0356c6a2bc58b5262d010e95
umtennis <umtennis@gmail.com>
2024-12-16 18:53:04 -0600

    fix style remove programs not offered

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,64 +15,70 @@ const About = () => {
         <div className="content-container">
           <div className="single-content-container">
 
-            <h1>About Us</h1>
-            <p>
-              The University of Manitoba Tennis Club (UMTC) welcomes both leisure and competitive players who meet our minimum skill level requirement, rating of 3.0 as outlined in the Tennis Canada Self-Rating Guide (<a href="https://www.tenniscanada.com/wp-content/uploads/2015/12/Self-Rating-Guide-English.pdf" target="_blank" rel="noopener noreferrer">View here</a>).
-            </p><p>
-              Tryouts are held at the beginning of each term to ensuring a great experience for all.
-            </p>
+            {/* Sponsors Section */}
+            <div className="section-layout">
+              <h2>About Us</h2>
+              <p className="section-message">
+                The University of Manitoba Tennis Club (UMTC) welcomes both leisure and competitive players who meet our minimum skill level requirement, rating of 3.0 as outlined in the Tennis Canada Self-Rating Guide (<a href="https://www.tenniscanada.com/wp-content/uploads/2015/12/Self-Rating-Guide-English.pdf" target="_blank" rel="noopener noreferrer">View here</a>).
+                <p></p>
+                Tryouts are held at the beginning of each term to ensuring a great experience for all.
+              </p>
+            </div>
+            <div className="section-layout">
+              <h2>Connect With Us</h2>
 
-
-            <h2>Connect With Us</h2>
-
-            
-            <p>
-              For more information, contact us at <a href="mailto:umtennis@gmail.com">umtennis@gmail.com</a>.              
-            </p>
-
-            <h2>Executive Members 2024</h2>
-            <div className="exec-members-container">
-              <div className="exec-members-info">
-                <p>Meet our dedicated executive team for the 2024 season:</p>
-                <ul>
-                  <li>Craig Hillier</li>
-                  <li>Mario Bayueno</li>
-                  <li>Jesse Shen</li>
-                  <li>Tony Weekes</li>
-                </ul>
-              </div>
-              <div className="exec-members-image">
-                <img
-                  src={execMembers}
-                  alt="Executive Members 2024"
-                />
+              <p className="section-message">
+                For more information, contact us at <a href="mailto:umtennis@gmail.com">umtennis@gmail.com</a>.
+              </p>
+            </div>
+            <div className="section-layout">
+              <h2>Executive Members 2024</h2>
+              <div className="exec-members-container">
+                <div className="exec-members-info">
+                  <p className="section-message">
+                    Meet our dedicated executive team for the 2024 season:
+                    <p></p>
+                    <ul>
+                      <li>Craig Hillier</li>
+                      <li>Mario Bayueno</li>
+                      <li>Jesse Shen</li>
+                      <li>Tony Weekes</li>
+                    </ul>
+                  </p>
+                </div>
+                <div className="exec-members-image">
+                  <img
+                    src={execMembers}
+                    alt="Executive Members 2024"
+                  />
+                </div>
               </div>
             </div>
+            <div className="section-layout">
+              <h2>General Members 2024</h2>
+              <p className="section-message">
+                Our general members are the heart of our club. Together, we make UMTC
+                a place where everyone can enjoy the sport of tennis.
+              </p>
 
-            <h2>General Members 2024</h2>
-            <p>
-              Our general members are the heart of our club. Together, we make UMTC
-              a place where everyone can enjoy the sport of tennis.
-            </p>
+              <img
+                src={generalMembers}
+                alt="General Members 2023-2024"
+                style={{ maxWidth: "100%", height: "auto", margin: "20px 0" }}
+              />
 
-            <img
-              src={generalMembers}
-              alt="General Members 2023-2024"
-              style={{ maxWidth: "100%", height: "auto", margin: "20px 0" }}
-            />
-
-            <h2>Past Seasons</h2>
-            <h3>2022-2023</h3>
-            <img
-              src={generalMembers2023}
-              alt="General Members 2023"
-              style={{ maxWidth: "100%", height: "auto", margin: "20px 0" }}
-            />
-
+              <h2>Past Seasons</h2>
+              2022-2023
+              <img
+                src={generalMembers2023}
+                alt="General Members 2023"
+                style={{ maxWidth: "100%", height: "auto", margin: "20px 0" }}
+              />
+            </div>
           </div>
         </div>
       </div>
-      <Footer /> 
+      <Footer />
     </div>
 
 
--- src/pages/Programs.js
+++ src/pages/Programs.js
@@ -21,7 +21,7 @@ const Programs = () => {
 
     } else if (program === 'waitList') {
       setWaitlistModalIsOpen(true)
-    } else if (program === 'introClass'){
+    } else if (program === 'introClass') {
       setIntroClassModalIsOpen(true)
     }
   }
@@ -37,97 +37,99 @@ const Programs = () => {
       <div className="home-container">
         <div className="content-container">
           <div className="single-content-container">
-            <h2>Programs</h2>
-            <div className="programs-grid">
-              {/* General Membership Section */}
-              <div className="program-card">
-                <h3 onClick={() => toggleSection("generalMembership")} className="program-title">
-                  General Membership
-                </h3>
-                {expandedSection === "generalMembership" && (
-                  <div className="program-description">
-                    <ul>
-                      <li>
-                        Fall/Winter spots are prioritized for returning members. Remaining spots go to UM students first, then open to UM alumni and staff.
-                      </li>
-                      <li>
-                        A minimum 3.0 rating is required, following the: <a href="https://www.tenniscanada.com/wp-content/uploads/2015/12/Self-Rating-Guide-English.pdf" target="_blank" rel="noopener noreferrer">Self-Rating Guide</a>.
-                      </li>
-                      <li>
-                        Indoor sessions are limited to 18 players (6 per court), with play based on similar skill levels.
-                      </li>
-                      <li>
-                        Tennis balls are provided; players must bring their own racquet and appropriate sportswear.
-                      </li>
-                      <li>
-                        Club events and practice clinics will be made available to members through out the season.
-                      </li>
-                      <li>
-                        Club fees: $100/year for alumni and staff, $30/year for students. Payable via E-Transfer to umtennis@gmail.com.
-                      </li>
-                      <li>
-                        Rec fees: $100/4-month term for UM alumni and staff, $63.5/4-month term for UM students. Payable at Rec Services at Active Living Center or online <a href="https://sportandrec.umanitoba.ca/UOFM/public/category/browse/WINTERCLUBS" target="_blank" rel="noopener noreferrer">here</a>.
-                      </li>
-                      <li>
-                        If interested, please fill out the waitlist below. We will contact you as soon as we can.
-                      </li>
-                    </ul>
-                    <Button
-                      variant="outline-primary"
-                      onClick={() => handleProgramClick('waitList')} >
-                      New Member Waitlist
-                    </Button>
-                    {waitlistModalIsOpen &&
-                      <WaitlistModal show={waitlistModalIsOpen} handleClose={() => setWaitlistModalIsOpen(false)}
-                      />
-                    }
-                  </div>
-                )}
-              </div>
 
-              {/* Competitive Student Team Section */}
-              <div className="program-card">
-                <h3 onClick={() => toggleSection("competitiveTeam")} className="program-title">
-                  Competitive Student Team
-                </h3>
-                {expandedSection === "competitiveTeam" && (
-                  <div className="program-description">
-                    <p>
-                      We offer a 12-member men's and women's student of UofM team. Tryouts are held at the beginning of each season (around September). A traveling team of 6-8 players from each team will compete in other provinces. For more information, please email <a href="mailto:umtennis@gmail.com">umtennis@gmail.com</a>.
-                    </p>
-                    <Button
-                      variant="outline-primary">
-                      Current Team Roster
-                    </Button>{' '}
-                    <Button
-                      variant="outline-primary"
-                      onClick={() => handleProgramClick('compTeam')}>
-                      Team Tryout Signup
-                    </Button>
-                    {compTeamModalIsOpen &&
-                      <CompTeamModal show={compTeamModalIsOpen} handleClose={() => setCompTeamModalIsOpen(false)}
-                      />
-                    }
-                  </div>
-                )}
+            <div className="section-layout">
+              <h2>Programs</h2>
+              <div className="programs-grid">
+                {/* General Membership Section */}
+                <div className="program-card">
+                  <h3 onClick={() => toggleSection("generalMembership")} className="program-title">
+                    General Membership
+                  </h3>
+                  {expandedSection === "generalMembership" && (
+                    <div className="program-description">
+                      <ul>
+                        <li>
+                          Fall/Winter spots are prioritized for returning members. Remaining spots go to UM students first, then open to UM alumni and staff.
+                        </li>
+                        <li>
+                          A minimum 3.0 rating is required, following the: <a href="https://www.tenniscanada.com/wp-content/uploads/2015/12/Self-Rating-Guide-English.pdf" target="_blank" rel="noopener noreferrer">Self-Rating Guide</a>.
+                        </li>
+                        <li>
+                          Indoor sessions are limited to 18 players (6 per court), with play based on similar skill levels.
+                        </li>
+                        <li>
+                          Tennis balls are provided; players must bring their own racquet and appropriate sportswear.
+                        </li>
+                        <li>
+                          Club events and practice clinics will be made available to members through out the season.
+                        </li>
+                        <li>
+                          Club fees: $100/year for alumni and staff, $30/year for students. Payable via E-Transfer to umtennis@gmail.com.
+                        </li>
+                        <li>
+                          Rec fees: $100/4-month term for UM alumni and staff, $63.5/4-month term for UM students. Payable at Rec Services at Active Living Center or online <a href="https://sportandrec.umanitoba.ca/UOFM/public/category/browse/WINTERCLUBS" target="_blank" rel="noopener noreferrer">here</a>.
+                        </li>
+                        <li>
+                          If interested, please fill out the waitlist below. We will contact you as soon as we can.
+                        </li>
+                      </ul>
+                      <Button
+                        variant="outline-primary"
+                        onClick={() => handleProgramClick('waitList')} >
+                        New Member Waitlist
+                      </Button>
+                      {waitlistModalIsOpen &&
+                        <WaitlistModal show={waitlistModalIsOpen} handleClose={() => setWaitlistModalIsOpen(false)}
+                        />
+                      }
+                    </div>
+                  )}
+                </div>
 
-              </div>
+                {/* Competitive Student Team Section */}
+                <div className="program-card">
+                  <h3 onClick={() => toggleSection("competitiveTeam")} className="program-title">
+                    Competitive Student Team
+                  </h3>
+                  {expandedSection === "competitiveTeam" && (
+                    <div className="program-description">
+                      <p>
+                        We offer a 12-member men's and women's student of UofM team. Tryouts are held at the beginning of each season (around September). A traveling team of 6-8 players from each team will compete in other provinces. For more information, please email <a href="mailto:umtennis@gmail.com">umtennis@gmail.com</a>.
+                      </p>
+                      <Button
+                        variant="outline-primary">
+                        Current Team Roster
+                      </Button>{' '}
+                      <Button
+                        variant="outline-primary"
+                        onClick={() => handleProgramClick('compTeam')}>
+                        Team Tryout Signup
+                      </Button>
+                      {compTeamModalIsOpen &&
+                        <CompTeamModal show={compTeamModalIsOpen} handleClose={() => setCompTeamModalIsOpen(false)}
+                        />
+                      }
+                    </div>
+                  )}
 
-              {/* Socials Night */}
-              <div className="program-card">
-                <h3 onClick={() => toggleSection("socialsNight")} className="program-title">
-                  Members Socials Night
-                </h3>
-                {expandedSection === "socialsNight" && (
-                  <div className="program-description">
-                    <p>
-                      New this season, we plan to organize social nights for general members in the forms of doubles nights, triple threat clinic and ladders program. Stay tuned as new events will be added to Club Schedule page.
-                    </p>
-                  </div>
-                )}
-              </div>
+                </div>
 
-              {/* UM Student Intro Sessions
+                {/* Socials Night */}
+                <div className="program-card">
+                  <h3 onClick={() => toggleSection("socialsNight")} className="program-title">
+                    Members Socials Night
+                  </h3>
+                  {expandedSection === "socialsNight" && (
+                    <div className="program-description">
+                      <p>
+                        New this season, we plan to organize social nights for general members in the forms of doubles nights, triple threat clinic and ladders program. Stay tuned as new events will be added to Club Schedule page.
+                      </p>
+                    </div>
+                  )}
+                </div>
+
+                {/* UM Student Intro Sessions
               <div className="program-card">
                 <h3 onClick={() => toggleSection("introClass")} className="program-title">
                   UM Student Free Intro Class
@@ -153,36 +155,36 @@ const Programs = () => {
               </div> */}
 
 
-              {/* Group Lessons Section */}
-              <div className="program-card">
-                <h3 onClick={() => toggleSection("groupLessons")} className="program-title">
-                  Group Lessons
-                </h3>
-                {expandedSection === "groupLessons" && (
-                  <div className="program-description">
-                    <p>
-                      If you're are UMTC member and interested in group lessons, please email <a href="mailto:umtennis@gmail.com">umtennis@gmail.com</a> for more information.
-                    </p>
-                  </div>
-                )}
-              </div>
+                {/* Group Lessons Section */}
+                {/* <div className="program-card">
+                  <h3 onClick={() => toggleSection("groupLessons")} className="program-title">
+                    Group Lessons
+                  </h3>
+                  {expandedSection === "groupLessons" && (
+                    <div className="program-description">
+                      <p>
+                        If you're are UMTC member and interested in group lessons, please email <a href="mailto:umtennis@gmail.com">umtennis@gmail.com</a> for more information.
+                      </p>
+                    </div>
+                  )}
+                </div> */}
 
-              {/* Private Lessons Section */}
-              <div className="program-card">
-                <h3 onClick={() => toggleSection("privateLessons")} className="program-title">
-                  Private Lessons
-                </h3>
-                {expandedSection === "privateLessons" && (
-                  <div className="program-description">
-                    <p>
-                    If you're are UMTC member and interested in private lessons, please email <a href="mailto:umtennis@gmail.com">umtennis@gmail.com</a> for more information.
-                    </p>
-                  </div>
-                )}
-              </div>
+                {/* Private Lessons Section */}
+                {/* <div className="program-card">
+                  <h3 onClick={() => toggleSection("privateLessons")} className="program-title">
+                    Private Lessons
+                  </h3>
+                  {expandedSection === "privateLessons" && (
+                    <div className="program-description">
+                      <p>
+                        If you're are UMTC member and interested in private lessons, please email <a href="mailto:umtennis@gmail.com">umtennis@gmail.com</a> for more information.
+                      </p>
+                    </div>
+                  )}
+                </div> */}
 
-              {/* Tennis Racquet Restringing */}
-              {/* <div className="program-card">
+                {/* Tennis Racquet Restringing */}
+                {/* <div className="program-card">
                 <h3 onClick={() => toggleSection("restringing")} className="program-title">
                   Tennis Racquet Restringing
                 </h3>
@@ -194,6 +196,7 @@ const Programs = () => {
                   </div>
                 )}
               </div> */}
+              </div>
             </div>
           </div>
         </div>
